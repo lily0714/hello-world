f8ac44d17eebf05c11720ba811b17d0ede64f428
陸映螢 <lilylily819@gmail.com>
2017-08-02 16:19:08 +0800

    Create insidepage.js

---
+++ insidepage.js
@@ -0,0 +1,16 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript"> 
+$(document).ready(function() { 
+$(".categoryho").click(function(){
+   $(".category").css({left:"-20px"});
+   $(".categoryho").hide();
+   $(".categoryhide").show();
+});
+$(".categoryhide").click(function(){
+   $(".category").css({left:"-200px"});
+   $(".categoryhide").hide();
+   $(".categoryho").show();
+});
+$("#lily0714-github-io").hide();
+});
+</script>
