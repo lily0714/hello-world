4948692a6524b043bcbcd248782cbf044611a164
陸映螢 <lilylily819@gmail.com>
2017-08-05 20:00:22 +0800

    Create indsidepage.js

---
+++ indsidepage.js
@@ -0,0 +1,13 @@
+$(document).ready(function() { 
+$(".categoryho").click(function(){
+   $(".category").css({left:"-20px"});
+   $(".categoryho").hide();
+   $(".categoryhide").show();
+});
+$(".categoryhide").click(function(){
+   $(".category").css({left:"-200px"});
+   $(".categoryhide").hide();
+   $(".categoryho").show();
+});
+$("#lily0714-github-io").hide();
+});
